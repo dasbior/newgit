0e1c07dbac7e40a80a5ab63edec89e90495bac98
adi <waroli.adi@gmail.com>
2025-10-05 15:55:50 +0700

    belajar

---
+++ main.js
@@ -0,0 +1,17 @@
+const firstName = 'adi'
+     const age = 42
+     let address = "dalegan"
+     const height = 165
+
+     
+     
+
+     console.log(firstName)
+     console.log(age)
+     console.log(height);
+     console.log(address);
+     console.log(age + 1)
+     console.log(age * 2 )
+     console.log(age <30 )
+     console.log(age >30 )
+     
